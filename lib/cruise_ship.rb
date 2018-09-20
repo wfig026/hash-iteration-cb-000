851a5502b1752d2a797653a1266ca575ebb0ca79
Will Figueiredo <will@wfigueiredo.com>
2018-09-20 03:28:13 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,9 +1,9 @@
 passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
+  suite_a: "Amanda Presley",
+  suite_b: "Seymour Hoffman",
+  suite_c: "Alfred Tennyson",
+  suite_d: "Charlie Chaplin",
+  suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
